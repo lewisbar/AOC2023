b68b672488944ed78343e666b3ccf81cf7b48f24
Lennart Wisbar <lewisbar.git@gmail.com>
2023-12-01 11:34:53 +0100

    Add day 1 framework

---
+++ AOC2023-Day1/AOC2023-Day1/AOC2023_Day1.h
@@ -0,0 +1,18 @@
+//
+//  AOC2023_Day1.h
+//  AOC2023-Day1
+//
+//  Created by LennartWisbar on 01.12.23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AOC2023_Day1.
+FOUNDATION_EXPORT double AOC2023_Day1VersionNumber;
+
+//! Project version string for AOC2023_Day1.
+FOUNDATION_EXPORT const unsigned char AOC2023_Day1VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AOC2023_Day1/PublicHeader.h>
+
+
