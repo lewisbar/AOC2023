52da2242f9dba1accad0b4be06ae874db33f9f49
Lennart Wisbar <lewisbar.git@gmail.com>
2023-12-01 12:30:43 +0100

    Create app in workspace

---
+++ AOC2023-Solutions/AOC2023-Solutions/AOC2023_SolutionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AOC2023_SolutionsApp.swift
+//  AOC2023-Solutions
+//
+//  Created by LennartWisbar on 01.12.23.
+//
+
+import SwiftUI
+
+@main
+struct AOC2023_SolutionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AOC2023-Solutions/AOC2023-Solutions/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AOC2023-Solutions
+//
+//  Created by LennartWisbar on 01.12.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
