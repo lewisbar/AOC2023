a750680381a96955caae4c1e9c018bb48fc1fbdb
Lennart Wisbar <lewisbar.git@gmail.com>
2023-12-03 02:13:44 +0100

    Filter for possible games

--- AOC2023-Day2/AOC2023_Day2Tests/AOC2023_Day2Tests.swift
+++ AOC2023-Day2/AOC2023_Day2Tests/AOC2023_Day2Tests.swift
@@ -9,6 +9,14 @@ import XCTest
 
 enum Day2 {
     enum Part1 {
+        static func possibleGames(outOf games: [Game], for cubeSet: Round) -> [Game] {
+            games.filter { game in
+                game.rounds.allSatisfy { round in
+                    round.red <= cubeSet.red && round.green <= cubeSet.green && round.blue <= cubeSet.blue
+                }
+            }
+        }
+
         static func parseLines(_ input: String) -> [Game] {
             input.components(separatedBy: .newlines).compactMap(parseLine)
         }
@@ -108,6 +116,59 @@ final class AOC2023_Day2Tests: XCTestCase {
             ]),
         ]
 
+        XCTAssertEqual(result, expectedResult)
+    }
+
+    func test_possibleGames_returnsCorrectGames() {
+        let games = [
+            Game(id: 1, rounds: [
+                Round(red: 4, green: 0, blue: 3),
+                Round(red: 1, green: 2, blue: 6),
+                Round(red: 0, green: 2, blue: 0)
+            ]),
+            Game(id: 2, rounds: [
+                Round(red: 0, green: 2, blue: 1),
+                Round(red: 1, green: 3, blue: 4),
+                Round(red: 0, green: 1, blue: 1)
+            ]),
+            Game(id: 3, rounds: [
+                Round(red: 20, green: 8, blue: 6),
+                Round(red: 4, green: 13, blue: 5),
+                Round(red: 1, green: 5, blue: 0)
+            ]),
+            Game(id: 4, rounds: [
+                Round(red: 3, green: 1, blue: 6),
+                Round(red: 6, green: 3, blue: 0),
+                Round(red: 14, green: 3, blue: 15)
+            ]),
+            Game(id: 5, rounds: [
+                Round(red: 6, green: 3, blue: 1),
+                Round(red: 1, green: 2, blue: 2)
+            ]),
+        ]
+
+        let cubeSet = Round(red: 12, green: 13, blue: 14)
+
+        let result = Day2.Part1.possibleGames(outOf: games, for: cubeSet)
+
+        let expectedResult = [
+            Game(id: 1, rounds: [
+                Round(red: 4, green: 0, blue: 3),
+                Round(red: 1, green: 2, blue: 6),
+                Round(red: 0, green: 2, blue: 0)
+            ]),
+            Game(id: 2, rounds: [
+                Round(red: 0, green: 2, blue: 1),
+                Round(red: 1, green: 3, blue: 4),
+                Round(red: 0, green: 1, blue: 1)
+            ]),
+            Game(id: 5, rounds: [
+                Round(red: 6, green: 3, blue: 1),
+                Round(red: 1, green: 2, blue: 2)
+            ]),
+        ]
+
+        XCTAssertEqual(result.map(\.id), expectedResult.map(\.id))
         XCTAssertEqual(result, expectedResult)
     }
 }
