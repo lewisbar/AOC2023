73fb3ab0d324004b82047969dbc78e6e85a29e08
Lennart Wisbar <lewisbar.git@gmail.com>
2023-12-03 01:41:04 +0100

    Day 2 part 1 parse input to Games array

--- AOC2023-Day2/AOC2023_Day2Tests/AOC2023_Day2Tests.swift
+++ AOC2023-Day2/AOC2023_Day2Tests/AOC2023_Day2Tests.swift
@@ -9,6 +9,10 @@ import XCTest
 
 enum Day2 {
     enum Part1 {
+        static func parseLines(_ input: String) -> [Game] {
+            input.components(separatedBy: .newlines).compactMap(parseLine)
+        }
+
         static func parseLine(_ input: String) -> Game? {
             let rawParts = input.components(separatedBy: ": ")
 
@@ -62,6 +66,47 @@ final class AOC2023_Day2Tests: XCTestCase {
             Round(red: 1, green: 2, blue: 6),
             Round(red: 0, green: 2, blue: 0)
         ])
+
+        XCTAssertEqual(result, expectedResult)
+    }
+
+    func test_parseLines_createsGameModels() {
+        let input = """
+        Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+        Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+        Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+        Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+        Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+        """
+
+        let result = Day2.Part1.parseLines(input)
+
+        let expectedResult = [
+            Game(id: 1, rounds: [
+                Round(red: 4, green: 0, blue: 3),
+                Round(red: 1, green: 2, blue: 6),
+                Round(red: 0, green: 2, blue: 0)
+            ]),
+            Game(id: 2, rounds: [
+                Round(red: 0, green: 2, blue: 1),
+                Round(red: 1, green: 3, blue: 4),
+                Round(red: 0, green: 1, blue: 1)
+            ]),
+            Game(id: 3, rounds: [
+                Round(red: 20, green: 8, blue: 6),
+                Round(red: 4, green: 13, blue: 5),
+                Round(red: 1, green: 5, blue: 0)
+            ]),
+            Game(id: 4, rounds: [
+                Round(red: 3, green: 1, blue: 6),
+                Round(red: 6, green: 3, blue: 0),
+                Round(red: 14, green: 3, blue: 15)
+            ]),
+            Game(id: 5, rounds: [
+                Round(red: 6, green: 3, blue: 1),
+                Round(red: 1, green: 2, blue: 2)
+            ]),
+        ]
 
         XCTAssertEqual(result, expectedResult)
     }
