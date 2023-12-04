3ef0424c7a82e22568b41b145be3d69e19833c51
Lennart Wisbar <lewisbar.git@gmail.com>
2023-12-04 08:34:00 +0100

    Parse whole input

--- Day4/Day4Tests/Day4Tests.swift
+++ Day4/Day4Tests/Day4Tests.swift
@@ -8,12 +8,18 @@
 import XCTest
 
 enum Part1 {
+    static func parseLines(_ input: String) -> [(winningNumbers: [Int], ownedNumbers: [Int])] {
+        input
+            .components(separatedBy: .newlines)
+            .map(parseLine)
+    }
+
     static func parseLine(_ input: String) -> (winningNumbers: [Int], ownedNumbers: [Int]) {
         let components = input
             .components(separatedBy: ": ")[1]
             .components(separatedBy: " | ")
             .map { $0.components(separatedBy: .whitespaces).compactMap(Int.init) }
-        
+
         return (components[0], components[1])
     }
 
@@ -34,6 +40,31 @@ final class Day4Tests: XCTestCase {
 
         XCTAssertEqual(result.winningNumbers, [41, 48, 83, 86, 17])
         XCTAssertEqual(result.ownedNumbers, [83, 86, 6, 31, 17, 9, 48, 53])
+    }
+
+    func test_parseLines_returnsArrayOfParsedLines() {
+        let input = """
+        Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+        Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+        Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+        Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+        Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+        Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+        """
+
+        let result = Part1.parseLines(input)
+
+        let expectedResult = [
+            ([41, 48, 83, 86, 17], [83, 86,  6, 31, 17,  9, 48, 53]),
+            ([13, 32, 20, 16, 61], [61, 30, 68, 82, 17, 32, 24, 19]),
+            ([ 1, 21, 53, 59, 44], [69, 82, 63, 72, 16, 21, 14,  1]),
+            ([41, 92, 73, 84, 69], [59, 84, 76, 51, 58,  5, 54, 83]),
+            ([87, 83, 26, 28, 32], [88, 30, 70, 12, 93, 22, 82, 36]),
+            ([31, 18, 13, 56, 72], [74, 77, 10, 23, 35, 67, 36, 11])
+        ]
+
+        XCTAssertEqual(result.map { $0.0 }, expectedResult.map { $0.0 })
+        XCTAssertEqual(result.map { $0.1 }, expectedResult.map { $0.1 })
     }
 
     func test_pointsForWinningNumberCount_calculatesCorrectPoints() {
