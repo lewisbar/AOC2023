60820043ab0bc4a2a529e4000ac7daeda754953a
Lennart Wisbar <lewisbar.git@gmail.com>
2023-12-04 06:41:20 +0100

    Filter winning numbers

--- Day4/Day4Tests/Day4Tests.swift
+++ Day4/Day4Tests/Day4Tests.swift
@@ -11,6 +11,10 @@ enum Part1 {
     static func points(for winningNumberCount: Int) -> Int {
         Int(pow(2, Double(winningNumberCount - 1)))
     }
+
+    static func winningNumbers(outOf myNumbers: [Int], possibleWinners: [Int]) -> [Int] {
+        myNumbers.filter(possibleWinners.contains)
+    }
 }
 
 final class Day4Tests: XCTestCase {
@@ -20,5 +24,14 @@ final class Day4Tests: XCTestCase {
         let results = counts.map(Part1.points)
 
         XCTAssertEqual(results, [1, 2, 4, 8, 16])
+    }
+
+    func test_winningNumbers_findsMatchingNumbers() {
+        let possibleWinners = [41, 48, 83, 86, 17]
+        let myNumbers = [83, 86, 6, 31, 17, 9, 48, 53]
+
+        let result = Part1.winningNumbers(outOf: myNumbers, possibleWinners: possibleWinners)
+
+        XCTAssertEqual(result, [83, 86, 17, 48])
     }
 }
