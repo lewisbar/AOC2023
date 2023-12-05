45a38bc3ead4d5606f66990ee8a63f4423b415b2
Lennart Wisbar <lewisbar.git@gmail.com>
2023-12-05 08:49:11 +0100

    Add `AgriMap` to manage known key-value pairs

--- Day5/Day5Tests/Day5Tests.swift
+++ Day5/Day5Tests/Day5Tests.swift
@@ -7,6 +7,18 @@
 
 import XCTest
 
+class AgriMap {
+    private let knownValues: [Int: Int]
+
+    init(knownValues: [Int : Int]) {
+        self.knownValues = knownValues
+    }
+
+    func value(for key: Int) -> Int {
+        knownValues[key] ?? key
+    }
+}
+
 enum Part1 {
     static func parseSeeds(_ input: String) -> [Int] {
         input
@@ -24,4 +36,29 @@ final class Day5Tests: XCTestCase {
 
         XCTAssertEqual(result, [79, 14, 55, 13])
     }
+
+    func test_agriMap_valueForKey_returnsValuesForKnownKeys() {
+        let agriMap = AgriMap(knownValues: [
+            5: 10,
+            6: 12,
+            7: 15
+        ])
+
+        let result = [5, 6, 7].map { agriMap.value(for: $0) }
+
+        XCTAssertEqual(result, [10, 12, 15])
+    }
+
+    func test_agriMap_valueForKey_returnsValuesForUnknownKeys() {
+        let agriMap = AgriMap(knownValues: [
+            5: 10,
+            6: 12,
+            7: 15
+        ])
+
+        let result = [1, 4, 8].map { agriMap.value(for: $0) }
+
+        XCTAssertEqual(result, [1, 4, 8])
+    }
+
 }
