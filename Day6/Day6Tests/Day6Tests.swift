d335b2def1e0db24277584a3d1b09d83c3d710ec
Lennart Wisbar <lewisbar.git@gmail.com>
2023-12-06 17:19:28 +0100

    Move day 6 part 1 to production

--- Day6/Day6Tests/Day6Tests.swift
+++ Day6/Day6Tests/Day6Tests.swift
@@ -6,54 +6,7 @@
 //
 
 import XCTest
-
-struct Race: Equatable {
-    let duration: Int
-    let recordDistance: Int
-}
-
-public enum Part1 {
-    public static func multipliedWaysToWin(from input: String) -> Int {
-        parse(input)
-            .map(waysToWin)
-            .map { $0.count }
-            .reduce(1, *)
-    }
-
-    static func parse(_ input: String) -> [Race] {
-        let timesAndDistances = input
-            .components(separatedBy: .newlines)
-            .map { $0.components(separatedBy: ":")[1] }
-            .map { $0.trimmingCharacters(in: .whitespaces) }
-            .map { $0.components(separatedBy: .whitespaces).compactMap(Int.init) }
-
-        let times = timesAndDistances[0]
-        let distances = timesAndDistances[1]
-
-        return times.enumerated().map { (index, time) in
-            Race(duration: time, recordDistance: distances[index])
-        }
-    }
-
-    static func waysToWin(_ race: Race) -> [Int] {
-        var speed = 0
-        var recordSpeeds = [Int]()
-
-        while speed < race.duration {
-            if distance(forSpeed: speed, duration: race.duration) > race.recordDistance {
-                recordSpeeds.append(speed)
-            }
-            speed += 1
-        }
-
-        return recordSpeeds
-    }
-
-    static func distance(forSpeed speed: Int, duration: Int) -> Int {
-        let remainingDuration = duration - speed
-        return speed * remainingDuration
-    }
-}
+@testable import Day6
 
 final class Day6Tests: XCTestCase {
     func test_distanceForPressingTime_returnsCorrectDistance() {
