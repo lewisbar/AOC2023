2a7eddc5baaf8bdc9570d6ec122cd71ce50e3684
Lennart Wisbar <lewisbar.git@gmail.com>
2023-12-06 11:23:49 +0100

    Calculate distance for speed i.e. pressing duration

--- Day6/Day6Tests/Day6Tests.swift
+++ Day6/Day6Tests/Day6Tests.swift
@@ -7,6 +7,19 @@
 
 import XCTest
 
+public enum Part1 {
+    static func distance(forSpeed speed: Int, duration: Int) -> Int {
+        let remainingDuration = duration - speed
+        return speed * remainingDuration
+    }
+}
+
 final class Day6Tests: XCTestCase {
-    
+    func test_distanceForPressingTime_returnsCorrectDistance() {
+        let distances = [0, 1, 2, 3, 4, 5, 6, 7]
+
+        let result = distances.map { Part1.distance(forSpeed: $0, duration: 7) }
+
+        XCTAssertEqual(result, [0, 6, 10, 12, 12, 10, 6, 0])
+    }
 }
